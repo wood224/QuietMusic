8388e22603416478f836f46481ad7c12658aaabb
yeshang18 <2500595148@qq.com>
2022-06-12 16:19:17 +0800

    部分user接口

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -7,7 +7,6 @@ import com.example.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.lang.model.element.ModuleElement;
 import java.time.LocalDateTime;
 
 @RestController
