d8199ff5ba9b0775a482a98f9135effebc1af032
wood224 <610518780@qq.com>
2022-06-12 15:11:12 +0800

    初步格式化

---
+++ front/src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h3> Home 组件</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+}
+</script>
+
+<style lang="less" scoped>
+</style>
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,19 +1,14 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../components/Home.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    redirect: '/home'
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/home',
+    component: Home
   }
 ]
 
