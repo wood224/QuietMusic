f11a69fe6a3331a875dbf7f99a5a9fac1e7c2460
yeshang18 <2500595148@qq.com>
2022-06-13 10:04:10 +0800

    资源放行

--- src/main/java/com/example/JmmusicApplication.java
+++ src/main/java/com/example/JmmusicApplication.java
@@ -3,6 +3,7 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @ServletComponentScan
