805221bccaea92aba38d15298aaeaab18c822838
yeshang18 <104137595+yeshang18@users.noreply.github.com>
2022-06-13 08:33:23 +0800

    Revert "V0.1"

--- src/main/java/com/example/JmmusicApplication.java
+++ src/main/java/com/example/JmmusicApplication.java
@@ -2,10 +2,8 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
-@ServletComponentScan
 public class JmmusicApplication {
 
     public static void main(String[] args) {
