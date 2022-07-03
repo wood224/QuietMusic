68474fd8ab2bf7646edb48c7cd49f22fc340049d
yeshang18 <2500595148@qq.com>
2022-07-03 19:37:00 +0800

    网易云接口

--- src/main/java/com/example/JmmusicApplication.java
+++ src/main/java/com/example/JmmusicApplication.java
@@ -3,7 +3,9 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ServletComponentScan
@@ -13,4 +15,8 @@ public class JmmusicApplication {
         SpringApplication.run(JmmusicApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
