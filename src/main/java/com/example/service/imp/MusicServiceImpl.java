fc8245be5bede9d6b3e824163935a72c12a6aab9
yeshang18 <2500595148@qq.com>
2023-01-06 13:55:15 +0800

    修改音乐保存内容

--- src/main/java/com/example/service/imp/MusicServiceImpl.java
+++ src/main/java/com/example/service/imp/MusicServiceImpl.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.example.common.R;
 import com.example.entity.Music;
+import com.example.entity.MusicDto;
 import com.example.entity.MusicRecord;
 import com.example.entity.User;
 import com.example.mapper.MusicMapper;
@@ -11,6 +12,7 @@ import com.example.mapper.UserMapper;
 import com.example.service.MusicRecordService;
 import com.example.service.MusicService;
 import com.example.service.UserService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,31 +24,25 @@ public class MusicServiceImpl extends ServiceImpl<MusicMapper,Music> implements 
     private MusicRecordService musicRecordService;
 
     @Transactional
-    public void saveMusic(Integer id,Integer userId){
-        Music music = super.getById(id);
+    public void saveMusic(MusicDto musicDto){
+        Music music = super.getById(musicDto.getId());
         if (music!=null){
+            music.setName(musicDto.getName());
+            music.setSingerName(musicDto.getSingerName());
+            music.setTime(musicDto.getTime());
+            music.setAlbum(musicDto.getAlbum());
             music.setViews(music.getViews()+1);
             super.updateById(music);
         }
         else{
             Music music1 = new Music();
-            music1.setId(id);
+            BeanUtils.copyProperties(musicDto,music1);
             super.save(music1);
         }
-        if(userId!=null) {
-            LambdaQueryWrapper<MusicRecord> lambdaQueryWrapper = new LambdaQueryWrapper<>();
-            lambdaQueryWrapper.eq(MusicRecord::getMusicId, id);
-            lambdaQueryWrapper.eq(MusicRecord::getUserId, userId);
-            MusicRecord musicRecord = musicRecordService.getOne(lambdaQueryWrapper);
-            if (musicRecord == null) {
-                MusicRecord musicRecord1 = new MusicRecord();
-                musicRecord1.setMusicId(id);
-                musicRecord1.setUserId(userId);
-                musicRecordService.save(musicRecord1);
-            } else {
-                musicRecord.setViews(musicRecord.getViews() + 1);
-                musicRecordService.updateById(musicRecord);
-            }
-        }
+        MusicRecord musicRecord = new MusicRecord();
+        musicRecord.setMusicId(music.getId());
+        musicRecord.setUserId(musicDto.getUserId());
+        musicRecordService.save(musicRecord);
+
     }
 }
